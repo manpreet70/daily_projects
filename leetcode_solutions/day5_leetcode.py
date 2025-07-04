62678a627f2c3347bf18d499fa21c54354577035
Manpreet Singh <manpreetsingh87238@gmail.com>
2025-07-04 15:49:04 -0700

    Add Leetcode Day 5 solution: Jump Game using greedy approach

--- leetcode_solutions/day5_leetcode.py
+++ leetcode_solutions/day5_leetcode.py
@@ -1,4 +1,12 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         numslen = len(nums)
-        
+        max_reach = 0
+
+        for i in range(numslen):
+            if i > max_reach:
+                return False
+            if i + nums[i] > max_reach:
+                max_reach = i + nums[i]
+                
+        return True
